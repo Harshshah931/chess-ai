4ddf69e66f1c08f26dc84f3defd3df76e5f3c0f2
Harshshah931 <harshnshahbd@gmail.com>
2026-07-08 13:31:47 +0530

    use SVG chess piece as logo

--- src/components/ui/AppLogo.tsx
+++ src/components/ui/AppLogo.tsx
@@ -7,13 +7,14 @@ interface AppLogoProps {
 
 export default function AppLogo({ size = 32, className }: AppLogoProps) {
   return (
-    <img
-      src="/images/app_logo.png"
-      alt="ChessAI Logo"
+    <svg
       width={size}
       height={size}
+      viewBox="0 0 100 100"
       className={className}
-      style={{ objectFit: 'contain' }}
-    />
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <text y="80" fontSize="80" textAnchor="middle" x="50">♛</text>
+    </svg>
   );
 }
