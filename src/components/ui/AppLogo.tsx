ec6da955726b96e4bf37504a39d86e97cd832093
Harshshah931 <harshnshahbd@gmail.com>
2026-07-08 13:16:08 +0530

    fix logo using img tag

--- src/components/ui/AppLogo.tsx
+++ src/components/ui/AppLogo.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import Image from 'next/image';
-
 interface AppLogoProps {
   size?: number;
   className?: string;
@@ -9,12 +7,13 @@ interface AppLogoProps {
 
 export default function AppLogo({ size = 32, className }: AppLogoProps) {
   return (
-    <Image
+    <img
       src="/assets/images/app_logo.png"
       alt="ChessAI Logo"
       width={size}
       height={size}
       className={className}
+      style={{ objectFit: 'contain' }}
     />
   );
 }
